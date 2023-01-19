11b7da93835ab1e6c08dea9855740ddcf5357096
Andhikagr <81626756+Andhikagr@users.noreply.github.com>
2023-01-20 01:13:05 +0700

    Add files via upload

---
+++ WaroengFastFood/lib/constant.dart
@@ -0,0 +1,56 @@
+const foodCategoryList = [
+  {
+    'imagePath': 'assets/pizza.svg',
+    'name': 'Pizza',
+  },
+  {
+    'imagePath': 'assets/sea-food.svg',
+    'name': 'Seafood',
+  },
+  {
+    'imagePath': 'assets/coke.svg',
+    'name': 'Minuman',
+  },
+];
+
+const popularFoodList = [
+  {
+    'imagePath': 'assets/chowmin.png',
+    'name': 'Spaghetti Udang Merah',
+    'berat': 'Berat 500 gram',
+    'bintang': '4.9'
+  },
+  {
+    'imagePath': 'assets/pizza-1.png',
+    'name': 'Pizza Rasa Keju',
+    'berat': 'Berat 200 gram',
+    'bintang': '4.6'
+  },
+  {
+    'imagePath': 'assets/salad.png',
+    'name': 'Waroeng Salad Original',
+    'berat': 'Berat 200 gram',
+    'bintang': '4.5'
+  },
+  {
+    'imagePath': 'assets/sandwhich.png',
+    'name': 'Sandwhich Tuna',
+    'berat': 'Berat 250 gram',
+    'bintang': '4.4'
+  },
+  {
+    'imagePath': 'assets/pizza.png',
+    'name': 'Pizza Rasa Strowberry',
+    'berat': 'Berat 540 gr',
+    'bintang': '4.1'
+  },
+];
+
+const ingredients = [
+  {
+    'imagePath': 'assets/tomato.png',
+  },
+  {
+    'imagePath': 'assets/onion.png',
+  }
+];
